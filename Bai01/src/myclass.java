9fab668a47dbad7bbe0cd4b200503af7ae9970ec
sugeru04 <hieulocker@gmail.com>
2024-09-23 20:57:14 +0700

    update proj Bai01 - add comments - done

--- Bai01/src/myclass.java
+++ Bai01/src/myclass.java
@@ -1,11 +1,17 @@
 
 public class myclass {
-
+	
+	/**
+	 * @access public
+	 * @param array
+	 * @return null
+	 */
 	public static void main(String[] args) {
 		// khai báo các biến
 		int a = 5;
 		int b = 7;
 		int c= a+b;
-		System.out.println("tổng là:"+c);	}
-
+		System.out.println("tổng là:"+c);
+	}
+		
 }
